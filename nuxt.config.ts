b0a811bbead225ff7501d4b46d54e650351079ab
Theo Diamantidis <diamaltho@gmail.com>
2023-04-21 00:16:11 +0300

    dev: add nuxt-icon component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
+    'nuxt-icon',
   ],
   content: {
     // https://content.nuxtjs.org/api/configuration
