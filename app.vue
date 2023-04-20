e16ada3fa3aa0db7b1ebc8bc3d78fa073f163711
Theo Diamantidis <diamaltho@gmail.com>
2023-04-20 23:30:54 +0300

    ui: fix purple backdrop covering footer in pages shorter than screen height

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-slate-50 min-h-screen">
-    <div class="absolute bg-[hsl(268,80%,95.5%)] w-full h-full" style="max-height: 70vh"></div>
-    <div class="relative z-10 max-w-5xl mx-auto">
+    <div class="absolute bg-[hsl(268,80%,95.5%)] w-full h-[70vh]"></div>
+    <div class="relative z-10 max-w-5xl mx-auto min-h-[70vh]">
       <LayoutHeader />
       <main>
         <NuxtPage />
