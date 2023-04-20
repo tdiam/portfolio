a7c4ed452f92258ec37a83ad5022dfcbdf470623
Theo Diamantidis <diamaltho@gmail.com>
2023-04-20 23:15:28 +0300

    refactor: move menu into a composable

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,33 +1,5 @@
 <script setup>
-const route = useRoute()
-
-const menu = computed(() => [
-  {
-    title: 'Home',
-    path: '/',
-    active: route.path === '/',
-  },
-  {
-    title: 'About',
-    path: '/about',
-    active: route.path === '/about',
-  },
-  {
-    title: 'Work',
-    path: '/projects',
-    active: route.path.startsWith('/projects'),
-  },
-  {
-    title: 'Side quests',
-    path: '/side-quests',
-    active: route.path.startsWith('/side-quests'),
-  },
-  {
-    title: 'Contact',
-    path: '/contact',
-    active: route.path === '/contact',
-  },
-])
+const menu = useMenu()
 </script>
 
 <template>
---
+++ composables/menu.ts
@@ -0,0 +1,31 @@
+export const useMenu = () => {
+  const route = useRoute()
+
+  return computed(() => [
+    {
+      title: 'Home',
+      path: '/',
+      active: route.path === '/',
+    },
+    {
+      title: 'About',
+      path: '/about',
+      active: route.path === '/about',
+    },
+    {
+      title: 'Work',
+      path: '/projects',
+      active: route.path.startsWith('/projects'),
+    },
+    {
+      title: 'Side quests',
+      path: '/side-quests',
+      active: route.path.startsWith('/side-quests'),
+    },
+    {
+      title: 'Contact',
+      path: '/contact',
+      active: route.path === '/contact',
+    },
+  ])
+}
