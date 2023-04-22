b3d5cce7d3a63e8d38389200495c830f0aeb75fc
Theo Diamantidis <diamaltho@gmail.com>
2023-04-22 20:24:33 +0300

    feat: add tag tooltips

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-defineProps(['tag'])
+import { initTooltips } from 'flowbite'
+
+const props = defineProps(['tag'])
+
+const { data } = useAsyncData(`tag:${props.tag}`, () => {
+  return queryContent(`/tags/skills/${props.tag}`).findOne()
+})
+
+onMounted(initTooltips)
 </script>
 
 <template>
   <a
+    :data-tooltip-target="data && `tag-${$.uid}-tooltip`"
     href=""
     :class="`
       inline-block px-2 bg-purple-900 border border-transparent
@@ -12,4 +21,16 @@ defineProps(['tag'])
       `">
     {{ tag }}
   </a>
+  <template v-if="data">
+    <div
+      role="tooltip"
+      :id="`tag-${$.uid}-tooltip`"
+      :class="`
+        absolute z-10 inline-block px-4 py-2
+        bg-white shadow rounded invisible opacity-0
+        transition-opacity duration-300 tooltip
+      `">
+      <ContentRenderer :value="data" class="prose prose-sm prose-p:font-medium" />
+    </div>
+  </template>
 </template>
---
+++ content/tags/skills/node.js.md
@@ -0,0 +1,5 @@
+---
+level: Senior
+---
+
+Node.js
