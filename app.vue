7962233016ceba1e5bc9abb3d9c487ac9311f570
Theo Diamantidis <diamaltho@gmail.com>
2023-04-24 01:41:49 +0300

    use purple box only in some pages

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const route = useRoute()
+const isShowBackdrop = computed(() => {
+  return route.path === '/' || route.path.match(/^\/projects\/./)
+})
+</script>
+
 <template>
-  <div class="bg-slate-50 min-h-screen">
-    <div class="absolute bg-[hsl(268,80%,95.5%)] w-full h-[70vh]"></div>
+  <div :class="`min-h-screen ${isShowBackdrop ? 'bg-slate-50' : 'bg-slate-100'}`">
+    <div
+      v-if="isShowBackdrop"
+      class="absolute bg-[hsl(268,80%,95.5%)] w-full h-[70vh]">
+    </div>
     <div class="relative z-10 max-w-5xl mx-auto min-h-[70vh]">
       <LayoutHeader />
       <main class="py-20">
