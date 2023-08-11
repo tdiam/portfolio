956a609423d286aa116c661a6c071d97f9f4892b
Theo Diamantidis <diamaltho@gmail.com>
2023-08-11 23:59:04 +0300

    ui: add shadow to bottom nav

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -5,7 +5,8 @@ const menu = useMenu()
 <template>
   <nav
     :class="`
-      fixed bottom-0 z-30 bg-white md:static md:bg-transparent
+      fixed bottom-0 z-30 md:static bg-white md:bg-transparent
+      shadow-[0_-2px_6px_2px_rgb(0_0_0_/_0.1)] md:shadow-none
       container flex flex-wrap items-center justify-between md:py-12
     `">
     <div class="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
