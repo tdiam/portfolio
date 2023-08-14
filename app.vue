a40e730b65c6372960d39158daabae9fc88b6f87
Theo Diamantidis <diamaltho@gmail.com>
2023-08-14 18:57:09 +0300

    tweak: backdrop height for mobile

--- app.vue
+++ app.vue
@@ -12,8 +12,11 @@ useHead({
 })
 
 const route = useRoute()
-const isShowBackdrop = computed(() => {
-  return route.path === '/' || route.path.match(/^\/projects\/(?!tag\/)./)
+const isHome = computed(() => {
+  return route.path === '/'
+})
+const isProjectPage = computed(() => {
+  return route.path.match(/^\/projects\/(?!tag\/)./)
 })
 </script>
 
@@ -21,14 +24,19 @@ const isShowBackdrop = computed(() => {
   <div
     :class="`
       min-h-screen pb-[calc(3.25rem+2px)] md:pb-0
-      ${isShowBackdrop ? 'bg-slate-50' : 'bg-slate-100'}
+      ${(isHome || isProjectPage) ? 'bg-slate-50' : 'bg-slate-100'}
     `"
     >
     <div
-      v-if="isShowBackdrop"
-      class="absolute bg-[hsl(268,80%,95.5%)] w-full h-[70vh]">
+      v-if="isHome || isProjectPage"
+      :class="`absolute bg-[hsl(268,80%,95.5%)] w-full ${isHome ? 'h-[32rem]' : 'h-[28rem]'} md:h-[70vh]`">
     </div>
-    <div class="relative z-10 max-w-5xl mx-auto min-h-[70vh]">
+    <div
+      :class="`
+        relative z-10 max-w-5xl mx-auto
+        ${''/* 100vh - footer height - nav height */}
+        min-h-[calc(100vh-14.75rem-(3.25rem+2px))] md:min-h-[70vh]
+      `">
       <LayoutHeader />
       <main class="px-3 py-10 md:px-0 md:py-20">
         <NuxtPage />
