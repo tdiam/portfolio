7140170b2335b5fd4a738dc50aadd9ac35748bdc
Theo Diamantidis <diamaltho@gmail.com>
2023-08-14 18:20:09 +0300

    wip: caching

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 import svgLoader from 'vite-svg-loader'
 
+const pageCacheOpts = {
+  swr: 1 * 60 * 60,
+  maxAge: 24 * 60 * 60,
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -19,6 +24,12 @@ export default defineNuxtConfig({
     plugins: [
       svgLoader(),
     ],
+  },
+  routeRules: {
+    '/': pageCacheOpts,
+    '/about': pageCacheOpts,
+    '/projects/**': pageCacheOpts,
+    '/contact': pageCacheOpts,
   },
   googleFonts: {
     download: false,
