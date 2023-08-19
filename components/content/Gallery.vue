a303c3c80a2b9a1976c0539c149851fc05bec265
Theo Diamantidis <diamaltho@gmail.com>
2023-08-19 21:01:34 +0300

    feat: add Gallery component for project pages

---
+++ components/content/Gallery.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="grid grid-flow-row items-center justify-center md:grid-flow-col md:space-x-12">
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </div>
+</template>
