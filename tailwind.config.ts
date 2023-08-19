f65e33c6cf0a8f6119cbb04e7f762d54d7e74193
Theo Diamantidis <diamaltho@gmail.com>
2023-08-19 21:00:44 +0300

    tweak: include .md files to tailwind content pool

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,11 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
+  content: [
+    './components/**/*.vue',
+    './content/**/*.md',
+    './pages/**/*.vue',
+  ],
   theme: {
     fontFamily: {
       sans: ['Raleway', ...defaultTheme.fontFamily.sans],
