862cc893e5e1a3f2d25f3a2ff4994a0d129223f2
Theo Diamantidis <diamaltho@gmail.com>
2023-08-19 22:22:26 +0300

    ui: widen about intro text in mobiles

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -25,7 +25,7 @@ useHead({
       </a>
     </div>
     <div class="mt-12 md:mt-16 grid md:grid-cols-[minmax(0,2fr),minmax(0,1fr)] gap-x-8 gap-y-16 justify-between items-start">
-      <AboutIntro class="container max-w-xs mx-auto text-center md:max-w-none md:mx-0 md:text-left"/>
+      <AboutIntro class="container mx-auto text-center md:mx-0 md:text-left"/>
       <AboutProfiles />
       <AboutTimelines />
       <Skills />
