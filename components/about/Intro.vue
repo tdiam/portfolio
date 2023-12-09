7a499b264ab9aced8d968f002f5e848b3bd5294f
Theo Diamantidis <diamaltho@gmail.com>
2023-12-10 00:20:07 +0200

    content: update about/intro section

--- components/about/Intro.vue
+++ components/about/Intro.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="text-slate-800 md:text-xl md:leading-relaxed">
     <p>
-      10 years of experience crafting professional, volunteer, and personal
-      software projects, primarily using web technologies.
-    </p>
-    <p class="mt-4">
-      Currently in Skydra, Greece.
+      Crafting professional, volunteer, and personal
+      software projects, primarily using web technologies, since 2012.
     </p>
   </div>
 </template>
